a18e26c825330a83066a0c0eef40fd33fdcc303a
Rajrup Das <rajrupofficialid@protonmail.com>
2025-04-16 16:08:26 +0000

    Day 25 (#9)
    Signed-off-by: rajrupdasofficial <rajrupofficialid@protonmail.com>

---
+++ day25.c
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for intervals
+typedef struct {
+  int start;
+  int end;
+} Interval;
+
+Interval *insertInterval(Interval *intervals, int intervalsSize,
+                         Interval newInterval, int *returnSize) {
+  // code here
+  Interval *result = (Interval *)malloc((intervalsSize + 1) * sizeof(Interval));
+  int idx = 0;
+
+  for (int i = 0; i < intervalsSize; i++) {
+    if (intervals[i].end < newInterval.start) {
+      result[idx++] = intervals[i];
+    } else if (intervals[i].start <= newInterval.end) {
+      newInterval.start = (newInterval.start < intervals[i].start)
+                              ? newInterval.start
+                              : intervals[i].start;
+      newInterval.end = (newInterval.end > intervals[i].end) ? newInterval.end
+                                                             : intervals[i].end;
+    } else {
+      result[idx++] = newInterval;
+      newInterval = intervals[i];
+    }
+  }
+  result[idx++] = newInterval;
+
+  *returnSize = idx;
+  return result;
+}
+
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    int N;
+    scanf("%d", &N);
+    Interval *intervals = (Interval *)malloc(N * sizeof(Interval));
+
+    for (int i = 0; i < N; i++) {
+      scanf("%d %d", &intervals[i].start, &intervals[i].end);
+    }
+
+    Interval newInterval;
+    scanf("%d %d", &newInterval.start, &newInterval.end);
+
+    int returnSize;
+    Interval *ans = insertInterval(intervals, N, newInterval, &returnSize);
+
+    printf("[");
+    for (int i = 0; i < returnSize; i++) {
+      printf("[%d,%d]", ans[i].start, ans[i].end);
+      if (i != returnSize - 1) {
+        printf(",");
+      }
+    }
+    printf("]\n");
+
+    free(intervals);
+    free(ans);
+
+    printf("~\n");
+  }
+  return 0;
+}
